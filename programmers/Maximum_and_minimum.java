915ade8e5c46bd324abb4bd0bcafb3b4b1d8deab
sue <todayilearn@naver.com>
2024-04-21 19:34:34 +0900

    최대값과 최솟값 소스 추가

--- programmers/Maximum_and_minimum.java
+++ programmers/Maximum_and_minimum.java
@@ -1,5 +1,30 @@
 public class Maximum_and_minimum {
     
-  
+    public static void main(String[] args) {
+		solution("2 1 3 4");
+        // solution("-1 -2 -3 -4");
+        // solution("-1 -1");
+	}
+
+	public static String solution(String s) {
+		String answer = "";
+
+		String[] numbers = s.split(" ");
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+
+		for (int i = 0; i < numbers.length; i++) {
+			int number = Integer.parseInt(numbers[i]);
+
+			min = Math.min(min, number);
+			max = Math.max(max, number);
+
+			System.out.println(max);
+
+		}
+		answer = min + " " + max;
+
+		return answer;
+	}
 
 }
