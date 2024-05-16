615cb3f523544ba5832eaf4d03bbeea565695054
sue <todayilearn@naver.com>
2024-05-16 20:45:26 +0900

    DescendingInteger 소스 추가

--- programmers/DescendingInteger.java
+++ programmers/DescendingInteger.java
@@ -1,19 +1,19 @@
+import java.util.Arrays;
+
 public class DescendingInteger {
     
     public static void main(String[] args) {
-		descendingInteger(118372);
+		descendingInteger(8000013060L);
 	}
 
 	public static long descendingInteger(long n) {
-		String str = Long.toString(n);
+		char[] charArr = Long.toString(n).toCharArray();
 
-		char[] newArr = new char[str.length()];
-		int cnt = str.length() - 1;
-		
-		for (int i = 0; i < str.length(); i++) {
-			newArr[cnt] = str.charAt(i);
-			cnt--;
-		}
-		return Long.parseLong(String.valueOf(newArr));
+		Arrays.sort(charArr);
+
+		StringBuilder sb = new StringBuilder(new String(charArr)).reverse();
+
+		return Long.parseLong(sb.toString());
 	}
+
 }
