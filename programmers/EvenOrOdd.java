4cf73eeb9c7b6f930356cfd47375ec36ba740fa9
sue <todayilearn@naver.com>
2024-05-30 18:49:54 +0900

    EvenOrOdd 소스추가

--- programmers/EvenOrOdd.java
+++ programmers/EvenOrOdd.java
@@ -1,3 +1,9 @@
 public class EvenOrOdd {
-    
+    public static void main(String[] args) {
+		evenOrOdd(4);
+	}
+	
+	public static String evenOrOdd(int num) {
+        return (num % 2 == 0) ? "Even" : "Odd";
+    }
 }
